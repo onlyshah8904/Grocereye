740ccb98505b8338f169e25fb05ee1e50221d9ae
onlyshah8904 <kevalcontrol@gmail.com>
2025-08-20 11:45:14 +0530

    updated scraper.py

--- scraper.py
+++ scraper.py
@@ -1,11 +1,14 @@
 # scraper.py
 from blinkit import search_in_active_session as search_blinkit
 from bigbasket import search_in_active_session as search_bigbasket
+# from swiggy import search_in_active_session as search_swiggy
 from threading import Thread
 
 def scraper(keyword, pincode):
     r1, r2 = [], []
     t1 = Thread(target=lambda: r1.extend(search_blinkit(keyword, pincode)))
     t2 = Thread(target=lambda: r2.extend(search_bigbasket(keyword, pincode)))
-    t1.start(); t2.start(); t1.join(); t2.join()
+    # t3 = Thread(target=lambda: r3.extend(search_swiggy(keyword, pincode)))
+    t1.start(); t2.start(); # t3.start()
+    t1.join(); t2.join(); # t3.join()
     return r1 + r2
