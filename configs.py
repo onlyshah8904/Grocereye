86923a4ff1eb99b4e21c9a4c4028fe8efa1fe2ae
onlyshah8904 <kevalcontrol@gmail.com>
2025-08-22 09:00:06 +0530

    new key

--- configs.py
+++ configs.py
@@ -1,3 +1,4 @@
-API_KEY="AIzaSyDBur80fP-rgXq2rwPzHVt--_e8XFjsrKo"
+# API_KEY="AIzaSyDBur80fP-rgXq2rwPzHVt--_e8XFjsrKo"
 # API_KEY = "AIzaSyB-q2I6fdnvHilTKoZQt2fe0nOs3vLqryw"
 # API_KEY="AIzaSyDvcLVxt6HmlqWLNlLEebHyeYM8EgqP-zE"
+API_KEY = "AIzaSyDh1gC-UT1sle_srgxObdZY2HN4PMCeUPo"
